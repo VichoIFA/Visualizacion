a6cea051b5ff366c4e15652c7454a3635075ce77
Vicente Fuentes <vicentedelaselva@gmail.com>
2024-10-02 18:32:11 -0300

    comienzo proyecto

--- proyecto/mayorista9/views.py
+++ proyecto/mayorista9/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pandas as pd 
+import os
 
-# Create your views here.
+directorio_actual = os.getcwd()
+nombre_archivo = "mayorista9/static/Caso 3-train -dificil.csv"
+ruta = os.path.join(directorio_actual, nombre_archivo)
+ruta = ruta.replace("\\", "/")
+fuente = pd.read_csv(ruta)
+df_html = fuente.head().to_html()
+
+def Index(request):
+    return HttpResponse(df_html)
+    #return render(request, 'graficomuestra.html')
